ef97f7b69050ee7de4949a53418213ff01956c4f
Şükrü Özdemir <sukruozdmr97@gmail.com>
2017-04-23 22:04:55 +0300

    Anasayfa resim biçimi değiştirildi

--- OnlineSatisProje.Web/Controllers/HesabimController.cs
+++ OnlineSatisProje.Web/Controllers/HesabimController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 using OnlineSatisProje.Core.Entities;
 using OnlineSatisProje.Data;
 using OnlineSatisProje.Web.Models;
@@ -11,23 +14,28 @@ namespace OnlineSatisProje.Web.Controllers
     [Authorize]
     public class HesabimController : BaseController
     {
-        private readonly IRepository<KullaniciAdresMapping> _kullaniciAdresRepository;
         private readonly IRepository<Adres> _adresRepository;
         private readonly IRepository<Ilce> _ilceRepository;
-        private readonly IRepository<Sehir> _sehirRepository;
+        private readonly IRepository<KullaniciAdresMapping> _kullaniciAdresRepository;
+        private readonly IRepository<Resim> _resimRepository;
         private readonly IRepository<Satici> _saticiRepository;
+        private readonly IRepository<Sehir> _sehirRepository;
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
 
         public HesabimController(IRepository<KullaniciAdresMapping> kullaniciAdresRepository,
             IRepository<Adres> adresRepository,
             IRepository<Ilce> ilceRepository,
-            IRepository<Sehir> sehirRepository, 
-            IRepository<Satici> saticiRepository)
+            IRepository<Sehir> sehirRepository,
+            IRepository<Satici> saticiRepository,
+            IRepository<Resim> resimRepository)
         {
             _kullaniciAdresRepository = kullaniciAdresRepository;
             _adresRepository = adresRepository;
             _ilceRepository = ilceRepository;
             _sehirRepository = sehirRepository;
             _saticiRepository = saticiRepository;
+            _resimRepository = resimRepository;
         }
 
         public ActionResult Index()
@@ -45,33 +53,68 @@ namespace OnlineSatisProje.Web.Controllers
 
         public ActionResult Siparislerim()
         {
-            return View(CurrentUser.Siparis.Where(x => !x.Silindi).OrderByDescending(x=>x.Tarih).ToList());
+            return View(CurrentUser.Siparis.Where(x => !x.Silindi).OrderByDescending(x => x.Tarih).ToList());
         }
 
         public ActionResult SaticiProfil()
         {
             if (!Satici)
-            {
                 return RedirectToAction("Index");
-            }
             var satici = _saticiRepository.Table.FirstOrDefault(s => s.KullaniciId == CurrentUser.Id);
+            if (satici?.LogoId == null) return View(satici);
+            var saticiResim = _resimRepository.GetById(satici.LogoId);
+            ViewData["SaticiResim"] = saticiResim;
             return View(satici);
         }
 
         [HttpPost]
         public ActionResult SaticiResimUpload(HttpPostedFileBase saticiresim)
         {
-            if(saticiresim == null) throw new ArgumentNullException(nameof(saticiresim));
+            if (saticiresim == null) throw new ArgumentNullException(nameof(saticiresim));
+            if (!Satici) return RedirectToAction("Index");
 
             try
             {
                 if (saticiresim.ContentLength > 0)
                 {
-                    
+                    var fileName = Path.GetFileName(saticiresim.FileName);
+                    if (fileName != null)
+                    {
+                        const string directoryPath = "~/Content/Images/SaticiProfilUploads";
+                        if (!Directory.Exists(Server.MapPath(directoryPath)))
+                            Directory.CreateDirectory(Server.MapPath(directoryPath));
+
+                        var mapFileName = DateTime.Now.ToString("MM/dd/yyyy");
+                        var fName = mapFileName + "-" + Guid.NewGuid().ToString().Substring(0, 5) + "-" + fileName;
+                        var fullPath = directoryPath + "/" + fName;
+                        var filePath = Path.Combine(Server.MapPath(fullPath));
+                        saticiresim.SaveAs(filePath);
+                        var str = fullPath.Substring(1);
+                        
+                        var resim = new Resim
+                        {
+                            ResimBinary = null,
+                            ResimPath = str,
+                            AltAttr = "",
+                            TitleAttr = "",
+                            Baslik = Path.GetFileName(saticiresim.FileName),
+                            CreatedDate = DateTime.Now
+                        };
+                        _resimRepository.Insert(resim);
+                        var satici = _saticiRepository.Table.FirstOrDefault(x => x.KullaniciId == CurrentUser.Id);
+                        if (satici != null)
+                        {
+                            satici.UpdatedDate = DateTime.Now;
+                            satici.LogoId = resim.Id;
+                            _saticiRepository.Update(satici);
+                            return RedirectToAction("SaticiProfil");
+                        }
+                    }
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Logger.Error(ex.Message);
                 ModelState.AddModelError("", @"Resim eklenirken bir hata oluştu");
             }
 
@@ -121,13 +164,14 @@ namespace OnlineSatisProje.Web.Controllers
 
         public JsonResult Ilceler(int id)
         {
-            var liste = _ilceRepository.Table.Where(x => x.SehirId == id).Select(x => new
-            {
-                id = x.Id,
-                ad = x.Ad
-            }).ToList();
+            var liste = _ilceRepository.Table.Where(x => x.SehirId == id)
+                .Select(x => new
+                {
+                    id = x.Id,
+                    ad = x.Ad
+                })
+                .ToList();
             return Json(liste, JsonRequestBehavior.AllowGet);
         }
-
     }
 }
--- OnlineSatisProje.Web/Controllers/HomeController.cs
+++ OnlineSatisProje.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Web.Helpers;
+using System.Web.Mvc;
 using OnlineSatisProje.Core.Entities;
 using OnlineSatisProje.Data;
 using OnlineSatisProje.Services.Interfaces;
@@ -9,23 +12,26 @@ namespace OnlineSatisProje.Web.Controllers
     public class HomeController : BaseController
     {
         private readonly IRepository<Urun> _repositoryUrun;
-        private readonly IUrunRepository _urunRepository;
+        private readonly IRepository<Resim> _resimRepository;
         private readonly ISaticiRepository _saticiRepository;
+        private readonly IUrunRepository _urunRepository;
 
         public HomeController(IRepository<Urun> repositoryUrun,
-            IUrunRepository urunRepository, 
-            ISaticiRepository saticiRepository)
+            IUrunRepository urunRepository,
+            ISaticiRepository saticiRepository,
+            IRepository<Resim> resimRepository)
         {
             _repositoryUrun = repositoryUrun;
             _urunRepository = urunRepository;
             _saticiRepository = saticiRepository;
+            _resimRepository = resimRepository;
         }
 
-        [OutputCache(Duration = 30, NoStore = true)]
         public ActionResult Index()
         {
             ViewData["UrunListe"] = _urunRepository.GetHomePageProducts();
             ViewData["SaticiListe"] = _saticiRepository.GetAvailableSaitiSaticis();
+            ViewData["Resimler"] = _resimRepository.Table.ToList();
             return View();
         }
 
