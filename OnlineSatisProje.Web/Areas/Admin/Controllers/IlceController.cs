a1080467421c0f0aa2606b8e89b0c15da8c96025
Şükrü Özdemir <sukruozdmr97@gmail.com>
2017-05-28 18:10:57 +0300

    İlçe-Şehir sayfaları

---
+++ OnlineSatisProje.Web/Areas/Admin/Controllers/IlceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using OnlineSatisProje.Core.Entities;
+using OnlineSatisProje.Data;
+
+namespace OnlineSatisProje.Web.Areas.Admin.Controllers
+{
+    public class IlceController : Controller
+    {
+        private readonly IRepository<Sehir> _sehirRepository;
+        private readonly IRepository<Ilce> _ilceRepository;
+
+        public IlceController(IRepository<Sehir> sehirRepository,
+            IRepository<Ilce> ilceRepository)
+        {
+            _sehirRepository = sehirRepository;
+            _ilceRepository = ilceRepository;
+        }
+
+        // GET: Admin/Ilce
+        public ActionResult Index()
+        {
+            var liste = _ilceRepository.Table.OrderBy(i => i.Ad).ToList();
+            ViewData["Sehirler"] = new SelectList(_sehirRepository.Table.OrderBy(s => s.Ad).ToList(), "Id", "Ad");
+            return View(liste);
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Ilce ilce)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", @"İlçe eklenemedi");
+                return RedirectToAction("Index");
+            }
+            ilce.CreatedDate = DateTime.Now;
+            _ilceRepository.Insert(ilce);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OnlineSatisProje.Web/Areas/Admin/Controllers/SehirController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using OnlineSatisProje.Core.Entities;
+using OnlineSatisProje.Data;
+
+namespace OnlineSatisProje.Web.Areas.Admin.Controllers
+{
+    public class SehirController : Controller
+    {
+        private readonly IRepository<Sehir> _sehirRepository;
+
+        public SehirController(IRepository<Sehir> sehirRepository)
+        {
+            _sehirRepository = sehirRepository;
+        }
+
+        // GET: Admin/Sehir
+        public ActionResult Index()
+        {
+            return View(_sehirRepository.Table.OrderBy(s => s.Ad).ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Sehir sehir)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", @"Şehir eklenemedi");
+                return RedirectToAction("Index");
+            }
+
+            sehir.CreatedDate = DateTime.Now;
+            _sehirRepository.Insert(sehir);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
